86be7bc542c94a714b9dd31addd2c2fed46bd850
anisul islam <anisulislam980340@gmail.com>
2023-12-27 22:02:52 +0600

    csrf token solved

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://mamar-bank-61jh.onrender.com/']
+CSRF_TRUSTED_ORIGINS = ['https://mamar-bank-61jh.onrender.com']
 
 
 
