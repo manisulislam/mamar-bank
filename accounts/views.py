5caa89a8dfb06e7e2b55c31b46b1d0fb754cbfdb
anisul islam <anisulislam980340@gmail.com>
2023-12-28 10:30:15 +0600

    logout butthon inside form update

--- accounts/views.py
+++ accounts/views.py
@@ -32,8 +32,6 @@ class UserLoginView(LoginView):
 
 class UserLogoutView(LogoutView):
     def get_success_url(self):
-        if self.request.user.is_authenticated:
-            logout(self.request)
         return reverse_lazy('home')
 
 
